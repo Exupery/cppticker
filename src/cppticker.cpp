ab389ccf01a8236d5e4c01a0f6e34e08551e4b34
Matthew Frost <mf@matthewfrost.com>
2012-06-10 15:05:56 -0400

    initial commit

---
+++ src/cppticker.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cppticker.cpp
+// Author      : Matthew Frost
+// Version     : 0.1
+// Copyright   : "BEER-WARE LICENSE"
+// Description : Stock ticker
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "gcc test" << endl;
+	return 0;
+}
