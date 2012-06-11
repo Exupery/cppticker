6734f6ba7edea65bcef2ce85706aae2bbe51b8ae
Matthew Frost <mf@matthewfrost.com>
2012-06-10 22:12:28 -0400

    member variables set

--- src/cppticker.cpp
+++ src/cppticker.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include "Instrument.h"
 using namespace std;
 
@@ -33,6 +34,10 @@ int main() {
 	}
 	in.close();
 
+	cout << "Symbol:\t" << i.getSymbol() << endl;
+	cout << "CID:\t" << i.getCID() << endl;
+	cout << "Last:\t" << "$" << i.getLast() << endl;
+
 	return 0;
 }
 
@@ -41,7 +46,6 @@ void parseData(std::string input, Instrument &i) {
 		std::string sym = parseSymbol(input);
 		if (sym.length() > 0) {
 			i.setSymbol(sym);
-			cout << i.getSymbol() << endl;
 		}
 	}
 
@@ -49,14 +53,13 @@ void parseData(std::string input, Instrument &i) {
 		std::string cid = parseCID(input);
 		if (cid.length() > 0) {
 			i.setCID(cid);
-			cout << i.getCID() << endl;
 		}
 	}
 
 	if (i.getLast() == 0.0) {
-		double quote = parseQuote(input, "358464");
+		double quote = parseQuote(input, i.getCID());
 		if (quote > 0.0) {
-			cout << "$" << quote << endl;
+			i.setLast(quote);
 		}
 	}
 }
@@ -78,11 +81,17 @@ std::string parseCID(std::string rawData) {
 
 double parseQuote(std::string rawData, std::string cid) {
 	double quote = 0.0;
-	std::string quoteLine = string("ref_")+cid+string("_l");
+	std::string quoteLine = string("ref_")+cid+string("_l\">");
 	size_t foundQuote = rawData.find(quoteLine);
 	if (foundQuote != string::npos) {
-		cout << rawData << endl;
-		//cout << "start: " << start << "\tend: " << end << endl;
+		size_t start = rawData.find(quoteLine);
+		std::string endString = "</span>";
+		size_t end = rawData.find(endString, start);
+		if (start != string::npos && end != string::npos) {
+			std::string quoteString = rawData.substr(start+quoteLine.length(), (end-start-quoteLine.length()));
+			std::istringstream dbl(quoteString);
+			dbl >> quote;
+		}
 	}
 	return quote;
 }
